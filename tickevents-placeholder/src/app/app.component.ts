ac3f9fc3846ddae7dd69b7b17b0672d11f4b0d0a
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-08-18 01:40:51 +0200

    Placeholder first version

--- tickevents-placeholder/src/app/app.component.ts
+++ tickevents-placeholder/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tickevents-placeholder';
+  title = 'Tickevents | Welcome';
 }
