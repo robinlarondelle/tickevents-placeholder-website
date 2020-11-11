e80552ce4bc967eb8610159b69a0f8bb7fbe1424
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-11-11 12:57:15 +0100

    Added and updated Google tags

--- tickevents-placeholder/src/app/app.component.ts
+++ tickevents-placeholder/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators'
+import { environment } from 'src/environments/environment';
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -9,10 +13,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   constructor(
+    private router: Router
   ) {
+    const navEndEvent$ = this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', environment.gtag , {
+        'page_path': e.urlAfterRedirects
+      });
+    });
   }
 
   onActivate(event) {
-    window.scroll(0,0);
-}
+    window.scroll(0, 0);
+  }
 }
