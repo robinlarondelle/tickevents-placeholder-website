854ef29822215419384a320249814a0ca46f8e38
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-11-12 16:03:04 +0100

    Updated Google Tag Manager

--- tickevents-placeholder/src/environments/environment.prod.ts
+++ tickevents-placeholder/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  gtag: 'UA-179952559-1'
 };
--- tickevents-placeholder/src/environments/environment.ts
+++ tickevents-placeholder/src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  gtag: 'G-VRC0J05KNL'
 };
 
 /*
