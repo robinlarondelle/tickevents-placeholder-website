5799da5d90085b0838bb768d832326bc23d7ce36
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-11-12 15:35:31 +0100

    Added Google Tag Manager

--- tickevents-placeholder/src/app/app.component.ts
+++ tickevents-placeholder/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators'
-import { environment } from 'src/environments/environment';
-declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -13,16 +9,7 @@ declare var gtag;
 export class AppComponent {
 
   constructor(
-    private router: Router
   ) {
-    const navEndEvent$ = this.router.events.pipe(
-      filter(e => e instanceof NavigationEnd)
-    );
-    navEndEvent$.subscribe((e: NavigationEnd) => {
-      gtag('config', environment.gtag , {
-        'page_path': e.urlAfterRedirects
-      });
-    });
   }
 
   onActivate(event) {
