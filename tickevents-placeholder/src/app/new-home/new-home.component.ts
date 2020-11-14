3058ffed8d8df5fb7f66e4cb5fc0f13cc97f6b4a
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-11-14 02:03:54 +0100

    Added Development environment GTM

--- tickevents-placeholder/src/app/new-home/new-home.component.ts
+++ tickevents-placeholder/src/app/new-home/new-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-new-home',
@@ -7,13 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewHomeComponent implements OnInit {
 
-	constructor() { }
+	constructor(
+		private title: Title
+	) { }
 
 	ngOnInit(): void {
-
-
+		this.title.setTitle("Tickevents | Home")
 	}
-
-
-
 }
